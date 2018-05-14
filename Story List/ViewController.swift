08259611cddfa95e0622c908fdb557bf1b990c7f
RalucaMihaela <ionescu.m.raluca@gmail.com>
2018-05-14 17:52:03 +0300

    Add Hello World project

--- Story List/ViewController.swift
+++ Story List/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnClick(_ sender: UIButton) {
+        print("Hello world");
+    }
+    
 }
 
